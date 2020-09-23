2fff81b45ac1b7205a84ed389758e751a2b5bc8f
FabitoProgrammer <60280523+narvaezfb@users.noreply.github.com>
2020-09-23 00:15:28 -0400

    Add project files.

---
+++ AverageUnitsShippedByEmployeeCSharp/AverageUnitsShippedByEmployee.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace AverageUnitsShippedByEmployeeCSharp
+{
+    partial class frmAverageUnitsShippedByEmployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBigDisplayEmployee3 = new System.Windows.Forms.TextBox();
+            this.txtBigDisplayEmployee2 = new System.Windows.Forms.TextBox();
+            this.txtBigDisplayEmployee1 = new System.Windows.Forms.TextBox();
+            this.lblAverageEmployee2 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.txtboxUserInput = new System.Windows.Forms.TextBox();
+            this.lblOverallAverage = new System.Windows.Forms.Label();
+            this.lblAverageEmployee3 = new System.Windows.Forms.Label();
+            this.lblAverageEmployee1 = new System.Windows.Forms.Label();
+            this.lblEmployee3 = new System.Windows.Forms.Label();
+            this.lblEmployee2 = new System.Windows.Forms.Label();
+            this.lblEmployee1 = new System.Windows.Forms.Label();
+            this.lblUnits = new System.Windows.Forms.Label();
+            this.lblDayCounter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBigDisplayEmployee3
+            // 
+            this.txtBigDisplayEmployee3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtBigDisplayEmployee3.Location = new System.Drawing.Point(496, 142);
+            this.txtBigDisplayEmployee3.Multiline = true;
+            this.txtBigDisplayEmployee3.Name = "txtBigDisplayEmployee3";
+            this.txtBigDisplayEmployee3.ReadOnly = true;
+            this.txtBigDisplayEmployee3.Size = new System.Drawing.Size(145, 141);
+            this.txtBigDisplayEmployee3.TabIndex = 24;
+            // 
+            // txtBigDisplayEmployee2
+            // 
+            this.txtBigDisplayEmployee2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtBigDisplayEmployee2.Location = new System.Drawing.Point(333, 142);
+            this.txtBigDisplayEmployee2.Multiline = true;
+            this.txtBigDisplayEmployee2.Name = "txtBigDisplayEmployee2";
+            this.txtBigDisplayEmployee2.ReadOnly = true;
+            this.txtBigDisplayEmployee2.Size = new System.Drawing.Size(138, 141);
+            this.txtBigDisplayEmployee2.TabIndex = 23;
+            // 
+            // txtBigDisplayEmployee1
+            // 
+            this.txtBigDisplayEmployee1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtBigDisplayEmployee1.Location = new System.Drawing.Point(162, 142);
+            this.txtBigDisplayEmployee1.Multiline = true;
+            this.txtBigDisplayEmployee1.Name = "txtBigDisplayEmployee1";
+            this.txtBigDisplayEmployee1.ReadOnly = true;
+            this.txtBigDisplayEmployee1.Size = new System.Drawing.Size(142, 141);
+            this.txtBigDisplayEmployee1.TabIndex = 22;
+            // 
+            // lblAverageEmployee2
+            // 
+            this.lblAverageEmployee2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAverageEmployee2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblAverageEmployee2.Location = new System.Drawing.Point(332, 297);
+            this.lblAverageEmployee2.Name = "lblAverageEmployee2";
+            this.lblAverageEmployee2.Size = new System.Drawing.Size(139, 24);
+            this.lblAverageEmployee2.TabIndex = 26;
+            // 
+            // btnExit
+            // 
+            this.btnExit.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnExit.Location = new System.Drawing.Point(496, 384);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 31;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnReset.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.BtnReset.Location = new System.Drawing.Point(376, 384);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(75, 23);
+            this.BtnReset.TabIndex = 30;
+            this.BtnReset.Text = "&Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnEnter.Location = new System.Drawing.Point(251, 384);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 29;
+            this.btnEnter.Text = "&Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // txtboxUserInput
+            // 
+            this.txtboxUserInput.Location = new System.Drawing.Point(219, 79);
+            this.txtboxUserInput.Name = "txtboxUserInput";
+            this.txtboxUserInput.Size = new System.Drawing.Size(70, 22);
+            this.txtboxUserInput.TabIndex = 18;
+            // 
+            // lblOverallAverage
+            // 
+            this.lblOverallAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOverallAverage.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblOverallAverage.Location = new System.Drawing.Point(162, 339);
+            this.lblOverallAverage.Name = "lblOverallAverage";
+            this.lblOverallAverage.Size = new System.Drawing.Size(479, 29);
+            this.lblOverallAverage.TabIndex = 28;
+            this.lblOverallAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAverageEmployee3
+            // 
+            this.lblAverageEmployee3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAverageEmployee3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblAverageEmployee3.Location = new System.Drawing.Point(496, 297);
+            this.lblAverageEmployee3.Name = "lblAverageEmployee3";
+            this.lblAverageEmployee3.Size = new System.Drawing.Size(145, 24);
+            this.lblAverageEmployee3.TabIndex = 27;
+            // 
+            // lblAverageEmployee1
+            // 
+            this.lblAverageEmployee1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAverageEmployee1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblAverageEmployee1.Location = new System.Drawing.Point(162, 297);
+            this.lblAverageEmployee1.Name = "lblAverageEmployee1";
+            this.lblAverageEmployee1.Size = new System.Drawing.Size(142, 24);
+            this.lblAverageEmployee1.TabIndex = 25;
+            // 
+            // lblEmployee3
+            // 
+            this.lblEmployee3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblEmployee3.Location = new System.Drawing.Point(518, 109);
+            this.lblEmployee3.Name = "lblEmployee3";
+            this.lblEmployee3.Size = new System.Drawing.Size(100, 17);
+            this.lblEmployee3.TabIndex = 21;
+            this.lblEmployee3.Text = "Employee 3";
+            // 
+            // lblEmployee2
+            // 
+            this.lblEmployee2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblEmployee2.Location = new System.Drawing.Point(354, 109);
+            this.lblEmployee2.Name = "lblEmployee2";
+            this.lblEmployee2.Size = new System.Drawing.Size(97, 17);
+            this.lblEmployee2.TabIndex = 20;
+            this.lblEmployee2.Text = "Employee 2";
+            // 
+            // lblEmployee1
+            // 
+            this.lblEmployee1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
+            this.lblEmployee1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblEmployee1.Location = new System.Drawing.Point(173, 109);
+            this.lblEmployee1.Name = "lblEmployee1";
+            this.lblEmployee1.Size = new System.Drawing.Size(102, 17);
+            this.lblEmployee1.TabIndex = 19;
+            this.lblEmployee1.Text = "Employee 1";
+            // 
+            // lblUnits
+            // 
+            this.lblUnits.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblUnits.Location = new System.Drawing.Point(159, 79);
+            this.lblUnits.Name = "lblUnits";
+            this.lblUnits.Size = new System.Drawing.Size(51, 17);
+            this.lblUnits.TabIndex = 17;
+            this.lblUnits.Text = "Units:";
+            // 
+            // lblDayCounter
+            // 
+            this.lblDayCounter.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblDayCounter.Location = new System.Drawing.Point(159, 43);
+            this.lblDayCounter.Name = "lblDayCounter";
+            this.lblDayCounter.Size = new System.Drawing.Size(51, 23);
+            this.lblDayCounter.TabIndex = 16;
+            this.lblDayCounter.Text = "Day 1";
+            // 
+            // frmAverageUnitsShippedByEmployee
+            // 
+            this.AcceptButton = this.btnEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBigDisplayEmployee3);
+            this.Controls.Add(this.txtBigDisplayEmployee2);
+            this.Controls.Add(this.txtBigDisplayEmployee1);
+            this.Controls.Add(this.lblAverageEmployee2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtboxUserInput);
+            this.Controls.Add(this.lblOverallAverage);
+            this.Controls.Add(this.lblAverageEmployee3);
+            this.Controls.Add(this.lblAverageEmployee1);
+            this.Controls.Add(this.lblEmployee3);
+            this.Controls.Add(this.lblEmployee2);
+            this.Controls.Add(this.lblEmployee1);
+            this.Controls.Add(this.lblUnits);
+            this.Controls.Add(this.lblDayCounter);
+            this.Name = "frmAverageUnitsShippedByEmployee";
+            this.Text = "Average Units Shipped By Emplyee";
+            this.Load += new System.EventHandler(this.frmAverageUnitsShippedByEmployee_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.TextBox txtBigDisplayEmployee3;
+        internal System.Windows.Forms.TextBox txtBigDisplayEmployee2;
+        internal System.Windows.Forms.TextBox txtBigDisplayEmployee1;
+        internal System.Windows.Forms.Label lblAverageEmployee2;
+        internal System.Windows.Forms.Button btnExit;
+        internal System.Windows.Forms.Button BtnReset;
+        internal System.Windows.Forms.Button btnEnter;
+        internal System.Windows.Forms.TextBox txtboxUserInput;
+        internal System.Windows.Forms.Label lblOverallAverage;
+        internal System.Windows.Forms.Label lblAverageEmployee3;
+        internal System.Windows.Forms.Label lblAverageEmployee1;
+        internal System.Windows.Forms.Label lblEmployee3;
+        internal System.Windows.Forms.Label lblEmployee2;
+        internal System.Windows.Forms.Label lblEmployee1;
+        internal System.Windows.Forms.Label lblUnits;
+        internal System.Windows.Forms.Label lblDayCounter;
+    }
+}
+
---
+++ AverageUnitsShippedByEmployeeCSharp/AverageUnitsShippedByEmployee.cs
@@ -0,0 +1,131 @@
+﻿/*Fabian Narvaez
+ * C# Project 
+ * two dimensional Array pratice 
+ * */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AverageUnitsShippedByEmployeeCSharp
+{
+    public partial class frmAverageUnitsShippedByEmployee : Form
+    {
+        //Variables
+        int employee = 0;
+        int day = 0;
+        const int numberOfDays = 7;
+        const int numberOfEmployee = 3;
+        double TotalEmploymentShipment;
+        double AverageByEmployee;
+        double totalOverAllEmployeeShipment;
+        double averageForAllEmployees;
+       
+        TextBox[] textBoxesUnitsEnteredByEmployees;
+        Label[] labelTotalAverageDisplayEmployees;
+        double[,] valuesEntered = new double [numberOfEmployee  , numberOfDays ];
+        public frmAverageUnitsShippedByEmployee()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAverageUnitsShippedByEmployee_Load(object sender, EventArgs e)
+        {
+            textBoxesUnitsEnteredByEmployees = new TextBox[] { txtBigDisplayEmployee1, txtBigDisplayEmployee2, txtBigDisplayEmployee3 };
+            labelTotalAverageDisplayEmployees = new Label[] { lblAverageEmployee1, lblAverageEmployee2, lblAverageEmployee3 };
+
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtboxUserInput.Text, out valuesEntered[employee, day]))
+            {
+                if (valuesEntered[employee, day] >= 0 && valuesEntered[employee, day] <= 100)
+                {
+                    textBoxesUnitsEnteredByEmployees[employee].Text += valuesEntered[employee, day].ToString() + System.Environment.NewLine;
+                    day += 1;
+                    txtboxUserInput.Clear();
+                    if (day == numberOfDays )
+                    {
+                        TotalEmploymentShipment = 0;
+                        for (int index = 0; index < numberOfDays ; index++)
+                        {
+                            TotalEmploymentShipment += valuesEntered[employee, index];
+                        }
+
+                        AverageByEmployee = Math.Round(TotalEmploymentShipment / numberOfDays,2);
+                        labelTotalAverageDisplayEmployees[employee].Text = AverageByEmployee.ToString();
+                        day = 0;
+                        employee ++;
+                       
+                       
+                    }
+                    lblDayCounter .Text= "Day " + (day + 1).ToString();
+                    if (employee == numberOfEmployee)
+                    {
+                        totalOverAllEmployeeShipment = 0;
+                        foreach (double values in valuesEntered)
+                        {
+                            totalOverAllEmployeeShipment += values;
+                        }
+
+                        averageForAllEmployees = Math.Round(totalOverAllEmployeeShipment / valuesEntered.Length,2);
+                        lblOverallAverage.Text = "Total Average: " + averageForAllEmployees.ToString();
+                        BtnReset.Focus();
+                        txtboxUserInput.ReadOnly = true;
+                        btnEnter.Enabled = false;
+                       
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("The Value Entered Must be between 1 and 100");
+                    txtboxUserInput.Clear();
+                    txtboxUserInput.Focus();
+                }
+
+            }
+            else
+                MessageBox.Show("The Value Entered Must be Numeric");
+            txtboxUserInput.Clear();
+            txtboxUserInput.Focus();
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            clearTextBoxes(textBoxesUnitsEnteredByEmployees);
+            clearLabels(labelTotalAverageDisplayEmployees);
+            lblDayCounter.Text = "Day";
+            txtboxUserInput.ReadOnly = false;
+            btnEnter.Enabled = true;
+            txtboxUserInput.Focus();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        public void clearTextBoxes(TextBox[] txtBoxArray)
+        {
+            foreach (TextBox textBox in txtBoxArray)
+            {
+                textBox.Text = string.Empty;
+            }
+        }
+        public void clearLabels(Label[] labelArray)
+        {
+            foreach (Label label in labelArray)
+            {
+               label.Text = string.Empty;
+            }
+        }
+    }
+}
